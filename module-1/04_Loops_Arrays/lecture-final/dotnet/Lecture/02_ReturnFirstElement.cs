6f43f41188c2b08aa76e66eb42d53cf764fddcf5
joe riggs <joe.riggs@techelevator.com>
2021-09-16 12:24:48 -0400

    exercises

---
+++ module-1/04_Loops_Arrays/lecture-final/dotnet/Lecture/02_ReturnFirstElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+        2. Return the first element of the array
+            TOPIC: Accessing Array Elements
+        */
+        public int ReturnFirstElement()
+        {
+                                //0    1     2   
+            int[] portNumbers = { 80, 8080, 443 };
+            //int[] portNumbers = new int[3] { 80, 8080, 443 }; //another, more verbose way
+            //int portNumbersLengths = portNumbers.Length;
+            return portNumbers[0];
+        }
+    }
+}
---
+++ module-1/04_Loops_Arrays/lecture-final/dotnet/Lecture/09_ReturnSumEveryOtherNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+        9. We want this loop to only count every other item starting at zero.
+        What needs to change in the loop for it to do that?
+        TOPIC: Looping Through Arrays
+
+            // { 4, 3, 4, 1, 4, 6 }; -> 12
+
+        */
+        public int ReturnSumEveryOtherNumber(int[] arrayToLoopThrough)
+        {
+            int sum = 0;
+
+            //     Start;       Keep going while         Increment by two;
+            for (int i = 0; i <arrayToLoopThrough.Length; i = i+2)
+            {
+                sum = sum + arrayToLoopThrough[i];
+            }
+
+            return sum;
+        
+         }
+    }
+}

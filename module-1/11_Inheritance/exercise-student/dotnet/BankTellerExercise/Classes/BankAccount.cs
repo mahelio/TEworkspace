166856a1ff8db83ab0b20254cf4b28023b8b200c
Mustafa Abuhilal <mhilalosu@gmail.com>
2021-09-28 22:35:05 -0400

    inheritance pt1 by Mustafa

--- module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/BankAccount.cs
+++ module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/BankAccount.cs
@@ -2,6 +2,30 @@
 {
     public class BankAccount
     {
+        public string AccountHolderName { get; private set; }
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; private set; } = 0;
+
+        public BankAccount(string accountHolderName, string accountNumber)
+        {
+            this.AccountHolderName = accountHolderName;
+            this.AccountNumber = accountNumber;
+        }
+        public BankAccount(string accountHolderName, string accountNumber, decimal balance)
+        {
+            this.AccountHolderName = accountHolderName;
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+        }
+        //withdrawl and deposit mthods
+        public decimal Deposit(decimal amountToDeposit)
+        {
+            return Balance += amountToDeposit;
+        }
+        public virtual decimal Withdraw(decimal amountToWithdraw)
+        {
+            return Balance -= amountToWithdraw;
+        }
 
     }
 }

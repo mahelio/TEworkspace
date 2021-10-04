4c335b31c216b2e935b3fd67ada02add8c7c7c23
joe riggs <joe.riggs@techelevator.com>
2021-10-04 12:04:37 -0400

    exercise or lecture

---
+++ module-1/16_FileIO_Reading_in/lecture-final/dotnet/ExceptionHandling/MathIsFun.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class MathIsFun
+    {
+
+        public int Divide(int num1, int num2)
+        {
+
+            try
+            {
+            //potentially could go awry
+               return num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                //handling the exception
+                Console.WriteLine($"dont divide by zero");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("something else happened");
+            }
+            finally
+            {
+                //cleanup connection/ resources/
+                Console.WriteLine("Always executes");
+            }
+
+            return 0;
+        }
+
+        public int Multiply(int num1, int num2){
+
+            Exception e = new Exception();
+            throw e;
+            return num1 * num2;
+
+        }
+
+        public void NotEnoughMoney()
+        {
+            ExceptionHandling.Exceptions.OverdraftException overdraftException = new Exceptions.OverdraftException("you broke", 0);
+            throw overdraftException;
+
+        }
+    }
+}
---
+++ module-1/16_FileIO_Reading_in/lecture-final/dotnet/Lecture/Aids/5 Summing Up Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture.Aids
+{
+    public static class SummingUpNumbers
+    {
+        public static void ReadFile()
+        {
+            // Reading in a file of numbers
+            string folder = Environment.CurrentDirectory;
+            string filename = "numbers.txt";
+            // get the full path
+            string fullpath = Path.Combine(folder, filename);
+            int sum = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(fullpath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string currentLine = sr.ReadLine();
+                        sum += int.Parse(currentLine);
+    
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}

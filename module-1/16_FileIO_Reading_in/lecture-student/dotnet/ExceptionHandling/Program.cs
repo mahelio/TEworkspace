8450bfbe958437dfe93fb82d0cf6893ae8cc6d41
joe riggs <joe.riggs@techelevator.com>
2021-10-04 08:48:07 -0400

    exercise or lecture

---
+++ module-1/16_FileIO_Reading_in/lecture-student/dotnet/ExceptionHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExceptionHandling.Exceptions;
+using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * By default, when an Exception is thrown, it will "bubble up" through the call stack until
+            * it reaches the main method and then will cause the program to exit and print a stacktrace
+            * to the standard output
+            *
+            * By using try/catch blocks, you can stop the Exception from exiting the method and provide
+            * code to handle it.
+            */
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ module-1/16_FileIO_Reading_in/lecture-student/dotnet/Lecture/Aids/4 Reading CSVs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lecture.Aids
+{
+    public static class ReadingCSVFiles
+    {
+        /*
+        * Often programmers need to read in csv files and work with each column of data
+        * individually. It usually involves reading files and doing string manipulation
+        * at the same time.
+        */
+        public static void ReadFile()
+        {
+            // Directory and file name
+            string directory = Environment.CurrentDirectory;
+            string filename = "words.csv";
+
+            // Full Path
+            string fullPath = Path.Combine(directory, filename);
+
+        }
+    }
+}

3443795fe507c8e2d129c8713853ed8d0af4d33f
Mustafa Abuhilal <mhilalosu@gmail.com>
2021-10-07 08:47:48 -0400

    FileIOwriting by Mustafa

--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FindAndReplace/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FindAndReplace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
@@ -6,7 +7,57 @@ namespace FindAndReplace
     {
 		public static void Main(string[] args)
 		{
-            Console.WriteLine("Hello World!");
+            //Prompt user for the search word
+            Console.WriteLine("Enter the word to search for: ");
+            string searchWord = Console.ReadLine();
+
+            //Prompt the user for word to replace the search word with
+            Console.WriteLine($"Enter the replacement word for the {searchWord}: ");
+            string newWord = Console.ReadLine();
+
+            //Prompt the user to enter the source file
+            Console.WriteLine($"Enter the source file to replace {searchWord} with {newWord}: \n");
+            string srcPath = Console.ReadLine();
+            bool srcIsValid = File.Exists(srcPath);
+
+            //Prompt the user for the destination file
+            Console.WriteLine("Enter the destination file path: \n");
+            string destPath = Console.ReadLine();
+
+            if (srcIsValid)
+            {
+                try
+                {
+                    // Open the existing file with the typo using a StreamReader
+                    using (StreamReader sr = new StreamReader(srcPath))
+                    {
+                        // Open a StreamWriter where we will output the file
+                        using (StreamWriter sw = new StreamWriter(destPath))
+                        {
+                            // For each line in the input file, read it in
+                            while (!sr.EndOfStream)
+                            {
+                                // Read an individual line
+                                string line = sr.ReadLine();
+
+                                // Replace the occurence of the word langauge with language
+                                string replacedLine = line.Replace(searchWord, newWord);
+
+                                // Write the new line to the output file
+                                sw.WriteLine(replacedLine);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("couldnt do it, sorry");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid file path for the source file");
+            }
         }
     }
 }
--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,40 @@ namespace FizzWriter
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter file path: \n");
+            string filePath = Console.ReadLine();
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    for (int i = 1; i < 301; i++)
+                    {
+                        string intPath = i.ToString();
+
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            sw.WriteLine("FizzBuzz");
+                        }
+                        else if (i % 5 == 0 || intPath.Contains('5'))
+                        {
+                            sw.WriteLine("Buzz");
+                        }
+                        else if (i % 3 == 0 || intPath.Contains('3'))
+                        {
+                            sw.WriteLine("Fizz");
+                        }
+                        else
+                        {
+                            sw.WriteLine($"{i}");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("file path was not valid.");
+            }
         }
     }
 }

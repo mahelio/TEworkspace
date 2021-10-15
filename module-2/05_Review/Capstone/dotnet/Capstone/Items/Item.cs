cc6adff5944ae83855212ed0c573e1d5facff374
joe riggs <joe.riggs@techelevator.com>
2021-10-15 10:42:54 -0400

    exercise or lecture

---
+++ module-2/05_Review/Capstone/dotnet/Capstone/Items/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public abstract class Item
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        protected int QuantityAvailable { get; set; }
+
+        public Item(string name, decimal price, int quantityToStock)
+        {
+            Name = name;
+            Price = price;
+            QuantityAvailable = quantityToStock;
+        }
+
+        public string QuanityAvailableMessage
+        {
+            get
+            {
+                if (QuantityAvailable > 0)
+                {
+                    return QuantityAvailable.ToString();
+                }
+                else
+                {
+                    return "Sold Out";
+                }
+            }
+        }
+
+        public abstract string VendItem();
+        public override string ToString()
+        {
+            return $"{Name} {Price} {QuanityAvailableMessage} ";
+        }
+    }
+}

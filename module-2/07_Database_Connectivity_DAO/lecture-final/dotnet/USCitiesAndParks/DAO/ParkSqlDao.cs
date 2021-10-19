f2d9e47a5a17a3090336334c1bb7c700bc7917e2
joe riggs <joe.riggs@techelevator.com>
2021-10-19 11:57:52 -0400

    exercise or lecture

---
+++ module-2/07_Database_Connectivity_DAO/lecture-final/dotnet/USCitiesAndParks/DAO/ParkSqlDao.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using USCitiesAndParks.Models;
+
+namespace USCitiesAndParks.DAO
+{
+    public class ParkSqlDao : IParkDao
+    {
+        private readonly string connectionString;
+
+        public ParkSqlDao(string connString)
+        {
+            connectionString = connString;
+        }
+
+        public Park GetPark(int parkId)
+        {
+            Park park = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+                conn.Open();
+                string sql = "SELECT park_name, date_established, area, has_camping FROM park WHERE park_id = @park_id;";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@park_id", parkId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    park = CreateParkFromReader(reader);
+                }
+
+                return park;
+
+            }
+
+
+        }
+
+        public IList<Park> GetParksByState(string stateAbbreviation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Park CreatePark(Park park)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePark(Park park)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePark(int parkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParkToState(int parkId, string state_abbreviation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveParkFromState(int parkId, string state_abbreviation)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Park CreateParkFromReader(SqlDataReader reader)
+        {
+            // park_name, date_established, area, has_camping 
+
+            Park park = new Park();
+            //park.ParkId = Convert.ToInt32(reader[""])
+            park.ParkName = Convert.ToString(reader["park_name"]);
+            park.DateEstablished = Convert.ToDateTime(reader["date_established"]);
+            park.HasCamping = Convert.ToBoolean(reader["has_camping"]);
+
+            return park;
+        }
+    }
+}
---
+++ module-2/07_Database_Connectivity_DAO/lecture-final/dotnet/USCitiesAndParks/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using USCitiesAndParks.DAO;
+
+namespace USCitiesAndParks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("UnitedStates");
+
+            //connection has server location, db name, credentials 
+            ICityDao cityDao = new CitySqlDao(connectionString);
+            IStateDao stateDao = new StateSqlDao(connectionString);
+            IParkDao parkDao = new ParkSqlDao(connectionString);
+
+            USCitiesAndParksCLI cli = new USCitiesAndParksCLI(cityDao, stateDao, parkDao);
+            cli.RunCLI();
+        }
+    }
+}

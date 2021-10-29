457552368fe94913e36f563a48989914a3da0c97
joe riggs <joe.riggs@techelevator.com>
2021-10-29 08:55:25 -0400

    exercise or lecture

---
+++ module-2/15_Review/EFandLINQ/EFandLINQ/Models/Park.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFandLINQ.Models
+{
+    public partial class Park
+    {
+        public Park()
+        {
+            ParkStates = new HashSet<ParkState>();
+        }
+
+        public int ParkId { get; set; }
+        public string ParkName { get; set; }
+        public DateTime DateEstablished { get; set; }
+        public decimal Area { get; set; }
+        public bool HasCamping { get; set; }
+
+        public virtual ICollection<ParkState> ParkStates { get; set; }
+    }
+}
---
+++ module-2/15_Review/EFandLINQ/EFandLINQ/Models/ParkState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFandLINQ.Models
+{
+    public partial class ParkState
+    {
+        public int ParkId { get; set; }
+        public string StateAbbreviation { get; set; }
+
+        public virtual Park Park { get; set; }
+        public virtual State StateAbbreviationNavigation { get; set; }
+    }
+}

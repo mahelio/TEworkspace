87ceae3320184402a32797c769892b0f8165d010
joe riggs <joe.riggs@techelevator.com>
2021-10-29 16:31:40 -0400

    exercise or lecture

---
+++ module-2/15_Review/EFandLINQ-final/EFandLINQ/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFandLINQ.Models
+{
+    public partial class City
+    {
+        public City()
+        {
+            States = new HashSet<State>();
+        }
+
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string StateAbbreviation { get; set; }
+        public int Population { get; set; }
+        public decimal Area { get; set; }
+
+        public virtual State StateAbbreviationNavigation { get; set; }
+        public virtual ICollection<State> States { get; set; }
+    }
+}

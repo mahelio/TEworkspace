619c06fad34f7f4961fe6da11d4aa5327704e3fc
joe riggs <joe.riggs@techelevator.com>
2021-11-16 09:08:29 -0500

    exercise or lecture

--- module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -13,6 +13,12 @@
         }
 */
 
+function sumDouble(x, y) {
+	// do logic here
+	// return result;
+	return x + y;
+}
+
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
     Given 3 int values, return true if 1 or more of them are teen.

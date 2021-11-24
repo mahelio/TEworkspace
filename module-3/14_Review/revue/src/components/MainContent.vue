00a3f8356ecbf2810e639182cfddc3f6db905232
joe riggs <joe.riggs@techelevator.com>
2021-11-24 14:39:39 -0500

    exercise or lecture

---
+++ module-3/14_Review/revue/src/components/MainContent.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <div class="content">
+      {{ name }}
+      <br />
+      {{ $store.state.content }}
+      <br />
+      {{ stateText }}
+      
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"
+            >Text 1</label
+          >
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            v-model="textBox"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <button v-on:click="handleClick" type="button" class="btn btn-primary">
+          Primary
+        </button>
+      </form>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "main-content",
+  data() {
+    return {
+      name: "Main Content",
+      textBox: ""
+    };
+  },
+  computed: {
+    stateText() {
+      return this.$store.state.content + "123";
+    },
+  },
+  methods: {
+    handleClick() {
+      console.log("clicked");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
